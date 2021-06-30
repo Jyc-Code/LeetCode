44090b0ed8865061ca4298a51cf62c1a3c654408
Jyc-Code <15888788015@163.com>
2021-06-30 09:59:45 +0800

    完成 209

--- array/209/209.c
+++ array/209/209.c
@@ -14,11 +14,12 @@ int minSubArrayLen(int target, int* nums, int numsSize)
 	int min = 0;
 	int sum = 0;
 
-	for(int i = 0,j = 0;i < numsSize;)
+	for(int i = 0,j = 0;i < numsSize && j < numsSize;)
 	{
-		if(i == 0)sum = nums[0];
-		else sum += nums[j];
+		if(i == j)sum = nums[j];
+		else if(minF == 0)sum += nums[j];
 
+		printf("i:%d j:%d sum:%d\n", i, j,sum);
 		if(sum >= target)
 		{
 			if((j - i) == 0)
@@ -28,26 +29,28 @@ int minSubArrayLen(int target, int* nums, int numsSize)
 			}
 			
 			printf("A%d %d\n",i,j);
-			sum -= nums[i++];
-			if(minF)
+			if(min != 0)
 				min = MIN(j - i + 1,min);
 			else
 				min = j - i + 1;
 
+			sum -= nums[i++];
 			minF = 1;
-			printf("B%d %d\n",i,j);
+			printf("B%d %d min:%d 2sum:%d\n",i,j,min,sum);
 		}
 		else
+		{
+			minF = 0;
 			j++;
+		}
 	}
 	return min;
 }
 
-
 int main(int argc, char *argv[])
 {
-	int nums[3] = {1, 4, 4};
-	int target = 4;
+	int nums[7] = {1, 1, 1, 1, 1,1 ,1};
+	int target = 11;
 	int numsSize = sizeof(nums)/sizeof(nums[0]);
 
 	printf("%d\n", minSubArrayLen(target, nums, numsSize));
